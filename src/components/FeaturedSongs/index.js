7007337451fe8c8b6051743622667e2b30f4dfc5
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-14 12:16:29 +0100

    move featured songs to own component

---
+++ src/components/FeaturedSongs/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { LyricsButton, FavButton } from "..";
+import './styles.css'
+
+const FeaturedSongs = () => {
+    const topSongs = [
+        { name: "Shame", plays: "9m" },
+        { name: "Only God Knows", plays: "4m" },
+        { name: "In My View", plays: "8m" }
+    ]
+
+    return (
+        <section className="top-songs">
+            <h2>Featured songs</h2>
+            <div className="songs">
+                <ol>
+                    {topSongs.map((song, index) =>
+                        <li key={index}>
+                            <span className="song-title">{song.name}</span>
+                            <span className="song-plays">{song.plays}</span>
+                            <LyricsButton songName={song.name} />
+                            <FavButton />
+                        </li>)}
+                </ol>
+            </div>
+        </section>
+    )
+}
+
+export default FeaturedSongs;
