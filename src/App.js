283e9df77068a8497b01b33e739acce70c928c07
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-15 18:28:52 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Switch, Route } from "react-router-dom";
 
-import { ArtistPortfolio, ArtistProfile } from "./components";
 import * as Layout from "./layout";
 import * as Pages from "./pages";
 
