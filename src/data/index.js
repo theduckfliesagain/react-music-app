16913669981f5341b76bd348ed732d0e60f378b8
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-15 14:19:45 +0100

    update data to include songs

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,73 @@
 
 const albums = [
-    { id: 0, title: "Cocoa Sugar", year: 2018, image: "https://f4.bcbits.com/img/a1022040919_10.jpg" },
-    { id: 1, title: "White Men Are Black Men Too", year: 2015, image: "https://f4.bcbits.com/img/a3109957338_10.jpg" },
-    { id: 2, title: "DEAD", year: 2014, image: "https://f4.bcbits.com/img/a0309814645_10.jpg" }
+   {
+      id: 0, title: "Cocoa Sugar", year: 2018,
+      image: "https://f4.bcbits.com/img/a1022040919_10.jpg",
+      songs: [
+         {
+            id: 1,
+            title: 'See How',
+            length: '2:01',
+         },
+         {
+            id: 2,
+            title: 'Fee Fi',
+            length: '2:41',
+         },
+         {
+            id: 3,
+            title: 'In My View',
+            length: '3:15',
+         },
+         {
+            id: 4,
+            title: 'Turn',
+            length: '3:36',
+         },
+         {
+            id: 5,
+            title: 'Lord',
+            length: '3:34',
+         },
+         {
+            id: 6,
+            title: 'Tremolo',
+            length: '3:08',
+         },
+         {
+            id: 7,
+            title: 'Wow',
+            length: '4:00',
+         },
+         {
+            id: 8,
+            title: 'Border Girl',
+            length: '4:01',
+         },
+         {
+            id: 9,
+            title: 'Holy Ghost',
+            length: '2:32',
+         },
+         {
+            id: 10,
+            title: 'Wire',
+            length: '1:40',
+         },
+         {
+            id: 11,
+            title: 'Toy',
+            length: '3:13',
+         },
+         {
+            id: 12,
+            title: 'Picking You',
+            length: '3:04',
+         },
+      ]
+   },
+   { id: 1, title: "White Men Are Black Men Too", year: 2015, image: "https://f4.bcbits.com/img/a3109957338_10.jpg" },
+   { id: 2, title: "DEAD", year: 2014, image: "https://f4.bcbits.com/img/a0309814645_10.jpg" }
 ]
 
 export default albums
