267e736d6a1c8ae7d7b9cd6c162a20b071f70f4f
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-15 11:04:47 +0100

    rename album component

--- src/components/AlbumCover/index.js
+++ src/components/AlbumCover/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
 
-const Album = ({album}) => {
+
+const Album = ({album, handleSelect}) => {
     return (
         <div key={album.title} className="album">
-            <img src={album.image} alt={album.title}></img>
+            <img src={album.image} alt={album.title}  onClick={() => handleSelect(album.id)}></img>
             <div className="info">
                 <h4>{album.year}</h4>
                 <h3>{album.title}</h3>
             </div>
         </div>
-    )
+    ) 
 }
 
 export default Album;
