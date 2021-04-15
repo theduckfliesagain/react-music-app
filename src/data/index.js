4455052771f59310bf3d8871c9519579db63b02b
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-15 11:05:27 +0100

    seperate data

---
+++ src/data/index.js
@@ -0,0 +1,8 @@
+
+const albums = [
+    { id: 0, title: "Cocoa Sugar", year: 2018, image: "https://f4.bcbits.com/img/a1022040919_10.jpg" },
+    { id: 1, title: "White Men Are Black Men Too", year: 2015, image: "https://f4.bcbits.com/img/a3109957338_10.jpg" },
+    { id: 2, title: "DEAD", year: 2014, image: "https://f4.bcbits.com/img/a0309814645_10.jpg" }
+]
+
+export default albums
