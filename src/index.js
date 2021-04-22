8d3dd87a918f4491d4936366bfd4c56534bec48f
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-22 11:12:16 +0100

    add theme provider

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store'
-
+import {ThemeProvider} from "./context/ThemeContext";
 import App from "./App";
 
 ReactDOM.render(
     <Router>
         <React.StrictMode>
             <Provider store={store}>
-                <App />
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
             </Provider>
         </React.StrictMode>
     </Router>,
